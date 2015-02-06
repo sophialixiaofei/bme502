7aaf862574259b57a2edcd4cf150479a4a29febb
hstrey <Helmut.Strey@gmail.com>
2015-02-06 12:13:36 -0500

    before class 2/6/2015

---
+++ homeworks/homework 1/hw1_kg.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  5 00:46:23 2015
+
+@author: kevinguerrero
+"""
+
+x=1
+y=x/2.0 + 1
+
+while y<2:
+    x=x/2.0
+    y=x/2.0 + 1
+else:
+    print y-1
+    
